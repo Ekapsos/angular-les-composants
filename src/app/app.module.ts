acca0069c8cd82734904f24097db0137b52b8abe
Ekapsos <133096165+Ekapsos@users.noreply.github.com>
2023-05-24 16:05:07 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BlockComponent } from './block/block.component';
+
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from "./app.component";
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+
+
+@NgModule({
+  declarations: [AppComponent, BlockComponent, UserProfileComponent],
+  imports: [BrowserModule,FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
